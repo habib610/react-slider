048def65e38e47e6d135860e8e8bf727a9f546e2
habib610 <habib54562@gmail.com>
2021-01-26 18:33:14 +0600

    slider-complete

---
+++ src/data.js
@@ -0,0 +1,32 @@
+const people = [
+    {
+        id: 1,
+        image: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" ,
+        name: "maria fergusion",
+        title: "office manger",
+        quote: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident praesentium sit repellat? Eius a aperiam eos dicta, distinctio hic dolores illum non ut.",
+    },
+    {
+        id: 2,
+        image: "https://images.pexels.com/photos/3988680/pexels-photo-3988680.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        title: "senior worker",
+        name: "sara johns",
+        quote: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi animi, quasi facilis vero repudiandae itaque?",
+    },
+    {
+        id: 3,
+        image: "https://images.pexels.com/photos/301958/pexels-photo-301958.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        name: "harley brown",
+        title: "assistant officer",
+        quote: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias veniam deleniti, aspernatur nisi similique quibusdam voluptatum tempora repudiandae. Velit iusto itaque nisi soluta dolorem facilis?",
+    },
+    {
+        id: 4,
+        image: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        name: "harley brown",
+        title: "assistant officer",
+        quote: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias veniam deleniti, aspernatur nisi similique quibusdam voluptatum tempora repudiandae. Velit iusto itaque nisi soluta dolorem facilis?",
+    }
+]
+
+export default people
